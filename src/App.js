c260b4782fe216f38e417e834726f4f16640a314
Luke <luke.michalek@gmail.com>
2023-01-08 19:27:26 +0000

    Add Links, NavLinks, Routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <HomePage />
-    </div>
+    <Router>
+
+      <Navigation />  
+
+      <Switch>
+        
+
+        <Route path='/:type/:id'>
+          <PetDetailsPage />
+        </Route>
+
+        <Route path="/:type?">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
